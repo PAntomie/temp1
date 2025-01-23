15e45feac6afb1073405bd45b881894a4047c4c5
P_Antomie <PhantomC261@outlook.com>
2025-01-23 16:22:10 +0800

    Initial commit

---
+++ src/client/java/cyn/otto/OttoClient.java
@@ -0,0 +1,10 @@
+package cyn.otto;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OttoClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/cyn/otto/OttoDataGenerator.java
@@ -0,0 +1,11 @@
+package cyn.otto;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OttoDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
